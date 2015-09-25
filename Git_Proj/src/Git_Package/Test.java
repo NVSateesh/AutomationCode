a569170e353a862c54abe7977ae927ae87f1138c
Sateesh <ksateesh@monocept.com>
2015-09-25 15:16:40 +0530

    Added Test Class to the Git_Package

---
+++ Git_Proj/src/Git_Package/Test.java
@@ -0,0 +1,16 @@
+package Git_Package;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+		System.out.println("Welcome to Git-1");
+		
+//		System.out.println("This is second line");
+
+	    }
+
+	}
+
+
+
