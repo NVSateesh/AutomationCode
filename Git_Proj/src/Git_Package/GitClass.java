d557e961930a5613e0ded4d63143f962629c0563
Sateesh <ksateesh@monocept.com>
2015-09-25 13:41:24 +0530

    Added

--- Git_Proj/src/Git_Package/GitClass.java
+++ Git_Proj/src/Git_Package/GitClass.java
@@ -4,6 +4,10 @@ public class GitClass {
     
     public static void main(String[] args){
 	System.out.println("ABCDEFGH");
+	
+	System.out.println("This is second line");
+	
+	System.out.println("This is Git line");
     }
 
 }
